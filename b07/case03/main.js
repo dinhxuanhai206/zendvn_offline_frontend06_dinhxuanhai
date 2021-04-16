7186b2761eb9bf93e805d26908c398d5964052bf
dinhxuanhai206@gmail.com <dinhxuanhai>
2021-04-16 19:29:28 +0700

    no message

---
+++ b07/case03/main.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var texts = document.getElementById('text').innerHTML;
+    document.getElementById('text').innerHTML = texts.replace(/frontend/gi, "<mark>Frontend</mark>");
+ 
+   
+}
+
+
